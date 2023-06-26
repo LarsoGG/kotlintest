ec9bcfb5010a605e29771eefaac2a6800ad9f4ff
LarsEs <107478638+LarsoGG@users.noreply.github.com>
2023-06-26 11:53:54 +0200

    Uploaded

---
+++ Test/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+var Name = "Nuttensohn"
+fun main(args: Array<String>) {
+    println("Hello World! du $Name")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+}
